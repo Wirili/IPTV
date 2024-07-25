f91477d0c0bb5765d37a1e7077e41bd2f48d11d1
Wu Ruilin <332477694@qq.com>
2024-07-25 09:26:10 +0800

    init

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ ip_version_priority = "ipv6"
 
 source_urls = [
     "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "http://120.79.4.185/new/mdlive.txt",
     "http://175.178.251.183:6689/channel.txt",
     # "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
-    "http://120.79.4.185/new/mdlive.txt",
     # "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
     # "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
     # "https://live.zhoujie218.top/tv/iptv6.txt",
