a3ebeac3115edb143c88db7e3e8fea51ba95f640
Wu Ruilin <332477694@qq.com>
2024-07-27 14:53:44 +0800

    init

--- main.py
+++ main.py
@@ -249,8 +249,7 @@ def getHotel():
             url,
             headers={
                 "Host": "www.foodieguide.com",
-                "Origin": "http://www.foodieguide.com",
-                "Referer": "http://www.foodieguide.com/iptvsearch/hotellist.html?s={0}&Submit=+".format(
+                "Referer": "http://www.foodieguide.com/iptvsearch/hotellist.html?s={0}".format(
                     item
                 ),
             },
