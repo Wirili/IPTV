a58375bdbe051fa28d8e317bb5db59eca9a57d27
Wirili <332477694@qq.com>
2024-11-03 08:48:24 +0800

    init

--- main.py
+++ main.py
@@ -327,7 +327,8 @@ def getHotelSearch(key):
             },
             headers={
                 "Host": "tonkiang.us",
-                "referer": "http://tonkiang.us/?"
+                "referer": "http://tonkiang.us/hoteliptv.php",
+                "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
             },
         )
 
