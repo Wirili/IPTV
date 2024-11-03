caa2555731a52ea125d4db49250f4a17fc02442e
Wirili <332477694@qq.com>
2024-11-03 10:32:48 +0800

    init

--- main.py
+++ main.py
@@ -369,15 +369,7 @@ def getHotelList(ip):
             url,
             headers={
                 "Host": "www.foodieguide.com",
-                "Referer": f"http://www.foodieguide.com/iptvsearch/hotellist.html?s={ip}"
-                "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
-            },
-        )
-        logging.info(f"url：{url} {rsp.text}")
-        rsp = requests.get(
-            url,
-            headers={
-                "Host": "www.foodieguide.com",
+                "Referer": f"http://www.foodieguide.com/iptvsearch/hotellist.html?s={ip}",
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
             },
         )
