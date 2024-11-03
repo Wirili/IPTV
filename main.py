54d45222df0bdd0c8a40c6715eb126ec08c99af1
Wirili <332477694@qq.com>
2024-11-03 11:19:23 +0800

    init

--- main.py
+++ main.py
@@ -380,16 +380,16 @@ def getHotelList(ip):
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
             },
         )
-        url = f"http://www.foodieguide.com/iptvsearch/testgo.php?s={ip}"
-        rsp = requests.get(
-            url,
-            headers={
-                "Host": "www.foodieguide.com",
-                "Referer": f"http://www.foodieguide.com/iptvsearch/hotellist.html?s={ip}",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            },
-        )
-        logging.info(f"url：{url} {rsp.text}")
+        # url = f"http://www.foodieguide.com/iptvsearch/testgo.php?s={ip}"
+        # rsp = requests.get(
+        #     url,
+        #     headers={
+        #         "Host": "www.foodieguide.com",
+        #         "Referer": f"http://www.foodieguide.com/iptvsearch/hotellist.html?s={ip}",
+        #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        #     },
+        # )
+        # logging.info(f"url：{url} {rsp.text}")
         url = f"http://www.foodieguide.com/iptvsearch/allllist.php?s={ip}&y=false"
         rsp = requests.get(
             url,
