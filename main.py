c20df443aa2be8a0fa6c3f03439ee9b80d6ec8ab
Wirili <332477694@qq.com>
2024-11-03 09:35:19 +0800

    init

--- main.py
+++ main.py
@@ -387,7 +387,6 @@ def getHotelList(ip):
             headers={
                 "Host": "tonkiang.us",
                 "Referer": f"http://tonkiang.us/hoteliptv.php",
-                "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
             },
         )
         logging.info(url + ":" + rsp.text)
@@ -398,7 +397,6 @@ def getHotelList(ip):
             headers={
                 "Host": "tonkiang.us",
                 "Referer": f"http://tonkiang.us/hotellist.html?s={ip}",
-                "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
             },
         )
 
@@ -409,7 +407,6 @@ def getHotelList(ip):
             headers={
                 "Host": "tonkiang.us",
                 "Referer": f"http://tonkiang.us/hotellist.html?s={ip}",
-                "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0"
             },
         )
 
