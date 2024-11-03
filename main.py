e9fb274bcc78cea8307a6a6583c94869acb28788
Wirili <332477694@qq.com>
2024-11-03 15:52:17 +0800

    init

--- main.py
+++ main.py
@@ -371,6 +371,13 @@ def getHotelList(ip):
     url = ""
     try:
         session = requests.Session()
+        rsp = session.get(
+            f"http://www.foodieguide.com/iptvsearch/",
+            headers={
+                "Host": "www.foodieguide.com",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            },
+        )
         lines = []
         url = f"http://www.foodieguide.com/iptvsearch/hotellist.html?s={ip}&Submit=+&y=y"
         rsp = session.get(
