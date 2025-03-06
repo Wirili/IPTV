de48255a7dc3c85f8a998993d94a167e669cd6b1
Wu Ruilin <332477694@qq.com>
2025-03-06 09:49:11 +0800

    init

--- config copy 2.py
+++ config copy 2.py
@@ -1,16 +1,27 @@
-ip_version_priority = ""
+ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://raw.githubusercontent.com/wwb521/live/main/tv.m3u",
-    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    "http://175.178.251.183:6689/aktvlive.txt",
+    "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
+    "https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/央视频道.txt",
+    "http://120.79.4.185/new/mdlive.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V4.txt",
+    "https://raw.githubusercontent.com/Fairy8o/IPTV/main/PDX-V6.txt",
     "https://live.zhoujie218.top/tv/iptv6.txt",
     "https://live.zhoujie218.top/tv/iptv4.txt",
+    "https://www.mytvsuper.xyz/m3u/Live.m3u",
+    "https://tv.youdu.fan:666/live/",
+    "http://ww.weidonglong.com/dsj.txt",
     "http://xhztv.top/zbc.txt",
-    #"https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
     "https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
     "http://home.jundie.top:81/Cat/tv/live.txt",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    "https://raw.githubusercontent.com/cymz6/AutoIPTV-Hotel/main/lives.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt",
     "https://fm1077.serv00.net/SmartTV.m3u",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
     "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
 ]
 
